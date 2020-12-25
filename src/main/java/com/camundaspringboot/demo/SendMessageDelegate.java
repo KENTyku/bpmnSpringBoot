86669dcc3e93814b46647967f873d2747a9bc54a
Yury Tveritin <ytveritin@inno.tech>
2020-12-25 17:47:08 +0400

    Create SendMessageDelegate

---
+++ src/main/java/com/camundaspringboot/demo/SendMessageDelegate.java
@@ -0,0 +1,22 @@
+package com.camundaspringboot.demo;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import static org.slf4j.LoggerFactory.getLogger;
+@Component
+public class SendMessageDelegate implements JavaDelegate {
+    private final Logger logger = getLogger(this.getClass());
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        String taskName=delegateExecution.getCurrentActivityName();
+        String name= delegateExecution.getBpmnModelElementInstance().getName();
+        logger.info("Service task {} is executed:", taskName);
+        String itemVariable = (String) delegateExecution.getVariable("item");
+        JavaKafkaProducerExample.sendMessage(itemVariable);
+        logger.info("Service task {} is done:", taskName);
+    }
+}
